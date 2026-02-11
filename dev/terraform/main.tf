f5c345557af2cb96eb8be3fcb177716dcfc8c149
Aditi Rajesh <111845100+aditirajesh@users.noreply.github.com>
2026-02-11 11:57:03 +0530

    change to main.tf to test out cross-repo workflow

--- dev/terraform/main.tf
+++ dev/terraform/main.tf
@@ -11,7 +11,7 @@ provider "null" {}
 
 resource "null_resource" "test" {
   provisioner "local-exec" {
-    command = "echo Terraform ran at $(date). Edit to test out cross-repo workflow"
+    command = "echo Terraform ran at $(date). Edit to test out cross-repo workflow. Change number 2"
   }
 }
 
